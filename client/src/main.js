897d495bf142093614a9f7622adfaf7760e843f4
xwills1337 <misha.sid@rambler.ru>
2026-05-27 13:15:20 +0400

    lab3

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,10 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import ImageLayer from 'ol/layer/Image';
-import ImageWMS from 'ol/source/ImageWMS';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import { Style, Fill, Stroke } from 'ol/style';
 
 const map = new Map({
     target: 'map',
@@ -14,22 +16,30 @@ const map = new Map({
         })
     ],
     view: new View({
-        center: [49.29494887590408, 53.59224334359169],
+        center: [49.2949, 53.5922],
         zoom: 18,
         projection: 'EPSG:4326'
     })
 });
 
-const wmsLayer = new ImageLayer({
-    source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/gis/wms',
-        params: {
-            LAYERS: 'gis:buildings',
-            TILED: true
-        },
-        ratio: 1,
-        serverType: 'geoserver'
-    })
-});
-
-map.addLayer(wmsLayer);
+fetch('/overture.geojson')
+    .then(response => response.json())
+    .then(data => {
+        const vectorLayer = new VectorLayer({
+            source: new VectorSource({
+                features: new GeoJSON().readFeatures(data)
+            }),
+            style: function (feature) {
+                const type = feature.get('source_type');
+                let color = '#cccccc';
+                if (type === 'my') color = '#00cc00';
+                else if (type === 'osm') color = '#3388ff';
+                else if (type === 'ml') color = '#ff8800';
+                return new Style({
+                    fill: new Fill({ color: color + '80' }),
+                    stroke: new Stroke({ color: '#ffffff', width: 1 })
+                });
+            }
+        });
+        map.addLayer(vectorLayer);
+    });
